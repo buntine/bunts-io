0b8b23cb74dee987fe1c3b5388c42a90a7098d90
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-01-26 18:16:18 +1100

    Added hiccup

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject bunts_io "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Personal website of Andrew Buntine"
+  :url "http://bunts.io"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler bunts-io.handler/app}
   :profiles
