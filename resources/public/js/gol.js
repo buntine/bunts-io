384d3f5c5b36f9c518c444cdffd390e7d9120dd8
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-01-27 00:46:44 +1100

    Finished basic implementation

--- resources/public/js/gol.js
+++ resources/public/js/gol.js
@@ -1,29 +1,28 @@
 (function () {
-  var w  = 50,
-      h  = 50,
+  var w  = 100,
+      h  = 100,
       cw = Math.round(document.body.clientWidth / w);
       ch = Math.round(document.body.clientHeight / h);
-      r  = 5000,
-      i  = [(4*w)+4, (5*w)+4, (6*w)+4, (6*w)+3, (5*w)+2],
-      s  = build_world();
+      r  = 100,
+      i  = [1045, 1145, 1046, 1047, 1048, 1148, 1248, 1348];
 
   function build_world() {
     var world = [],
         alive;
 
     for (var n=0; n<(w*h); n++) {
-      alive = i.indexOf(n) > 0 ? 1 : 0;
+      alive = i.indexOf(n) >= 0 ? 1 : 0;
       world.push(alive);
     }
 
     return world;
   }
 
-  function neighbours(c) {
-    return s[c-1]   + s[c+1]   +
-           s[c-w]   + s[c+w]   +
-           s[c-w-1] + s[c-w+1] +
-           s[c+w-1] + s[c+w+1];
+  function neighbours(c, s) {
+    return s[c-1]     + s[c+1]     +
+           s[c-w]     + s[c+w]     +
+           s[c-(w-1)] + s[c-(w+1)] +
+           s[c+(w-1)] + s[c+(w+1)];
   }
 
   function clearWorld() {
@@ -36,32 +35,30 @@
 
   function draw(c) {
     var cx = c % w,
-        cy = Math.round(c / h),
+        cy = Math.trunc(c / h),
         x  = cw * cx,
         y  = ch * cy;
 
-    document.write("<div class='cell' style='position:absolute;top:" + y + "px;left:" + x + "px;width:" + cw + "px;height:" + ch + ";background-color: #ccc;'>" + c + "</div>");
+    document.write("<div class='cell' style='position:absolute;top:" + y + "px;left:" + x + "px;width:" + cw + "px;height:" + ch + ";background-color: #ccc;'></div>");
   }
 
-  (function step() {
-    var sum;
+  (function step(state) {
+    var sum,
+        new_state = [];
 
     clearWorld();
 
-    for (var cell=0; cell<(w*h); cell++) {
-      sum = neighbours(cell);
-if (cell == 609) {
-console.log(sum);
-}
-
-      if (sum < 2 || sum > 3) {
-        s[cell] = 0;
-      } else if (sum == 2 || sum == 3) {
-        s[cell] = 1;
+    for (var cell=0; cell<state.length; cell++) {
+      sum = neighbours(cell, state);
+      if (state[cell] == 1 && (sum < 2 || sum > 3)) {
+        new_state[cell] = 0;
+      } else if ((state[cell] == 1 && (sum == 2 || sum == 3)) || (state[cell] == 0 && sum == 3)) {
+        new_state[cell] = 1;
         draw(cell);
+      } else {
+        new_state[cell] = state[cell];
       }
     }
-
-    setTimeout(step, r);
-  })();
+    setTimeout(function(){step(new_state);}, r);
+  })(build_world());
 }());
