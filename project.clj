8e695b59b05f076e6bf5ab7dad0017082c3868d2
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-02-05 19:15:06 +1100

    Setting up for deploymeny

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-beanstalk "0.2.7"]]
   :ring {:handler bunts-io.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
