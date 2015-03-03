c0e5f844466fb0459343fbb02fe886254b1a5797
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-03-03 21:43:31 +1100

    Fucking around with gradients

--- resources/public/js/gol.js
+++ resources/public/js/gol.js
@@ -1,18 +1,23 @@
 (function () {
   var w      = 110,
       h      = 82,
-      r      = 80,
+      r      = 60,
       cvs    = document.getElementById("gol"),
       ctx    = cvs.getContext("2d"),
       seed   = [((w * 20) + 50), ((w * 21) + 52), ((w * 22) + 49),
              ((w * 22) + 50), ((w * 22) + 53), ((w * 22) + 54), ((w * 22) + 55)],
       glider = [0, (w + 1), (w + 2), (w * 2), (w * 2 + 1)],
       mixin  = [],
+      start_colour = 9496061,
+      end_colour   = 882784,
+      colours = ["90E5FD", "8DE2F9", "8AE0F6", "87DEF3", "85DCF0", "82D9EC", "7FD7E9", "7DD5E6", "7AD3E3", "77D0E0", "75CEDC", "72CCD9", "6FCAD6", "6DC8D3", "6AC5D0", "67C3CC", "65C1C9", "62BFC6", "5FBCC3", "5DBAC0", "5AB8BC", "57B6B9", "55B4B6", "52B1B3", "4FAFB0", "4DADAC", "4AABA9", "47A8A6", "45A6A3", "42A4A0", "3FA29C", "3DA099", "3A9D96", "379B93", "359990", "32978C", "2F9489", "2D9286", "2A9083", "278E80", "258C7C", "228979", "1F8776", "1D8573", "1A8370", "17806C", "157E69", "127C66", "0F7A63", "0D7860"],
+      colour = 0,
       cw,
       ch;
 
   window.addEventListener("resize", resize_canvas, false);
   resize_canvas();
+  setInterval(update_colour, 40);
 
   document.getElementById("content").addEventListener("click", function(e) {
     var cell = cell_for(e.x, e.y);
@@ -33,6 +38,20 @@
     ch = ((window.innerHeight - 10) / h).toFixed(1);
   }
 
+  function update_colour() {
+    colour += 1
+
+    if (colour >= colours.length) {
+      colour = 0;
+    }
+  }
+
+  function colour_for_population(alive) {
+    var normalised = Math.round(((alive - 1) / (((w*h) / 10) - 1)) * 50);
+
+    return colours[normalised];
+  }
+
   function build_world() {
     var world = [];
 
@@ -50,14 +69,14 @@
            s[c+w-1] + s[c+w+1];
   }
 
-  function draw(c, colour) {
+  function draw(c) {
     var cx     = c % w,
         cy     = Math.trunc(c / w),
         x      = cw * cx,
         y      = ch * cy;
 
     ctx.beginPath();
-    ctx.fillStyle   = "#" + colour;
+    ctx.fillStyle   = "#" + colours[colour];
     ctx.lineWidth   = 1;
     ctx.strokeStyle = "#fff";
     ctx.fillRect(x, y, cw, ch);
@@ -66,7 +85,8 @@
 
   (function step(state) {
     var sum,
-        new_state = [];
+        new_state = [],
+        alive = 0;
 
     ctx.clearRect (0, 0, cvs.width, cvs.height);
 
@@ -74,7 +94,8 @@
       sum = neighbours(c, state);
 
       if (state[c] == 1) {
-        draw(c, "afaaf4");
+        alive += 1;
+        draw(c);
       }
 
       if (sum == 2) {
@@ -90,6 +111,8 @@
       fjs.each(function(c){ new_state[c] = 1; }, mixin);
       mixin = [];
     }
+
+    //colour = colour_for_population(alive);
 
     setTimeout(function(){step(new_state);}, r);
   })(build_world());
