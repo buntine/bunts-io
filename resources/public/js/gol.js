952f3d918f7d5bfe72c7d16d5e63f7e3d003c1c1
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-03-03 21:52:30 +1100

    Setup basic pulsating in gradient

--- resources/public/js/gol.js
+++ resources/public/js/gol.js
@@ -10,14 +10,14 @@
       mixin  = [],
       start_colour = 9496061,
       end_colour   = 882784,
-      colours = ["90E5FD", "8DE2F9", "8AE0F6", "87DEF3", "85DCF0", "82D9EC", "7FD7E9", "7DD5E6", "7AD3E3", "77D0E0", "75CEDC", "72CCD9", "6FCAD6", "6DC8D3", "6AC5D0", "67C3CC", "65C1C9", "62BFC6", "5FBCC3", "5DBAC0", "5AB8BC", "57B6B9", "55B4B6", "52B1B3", "4FAFB0", "4DADAC", "4AABA9", "47A8A6", "45A6A3", "42A4A0", "3FA29C", "3DA099", "3A9D96", "379B93", "359990", "32978C", "2F9489", "2D9286", "2A9083", "278E80", "258C7C", "228979", "1F8776", "1D8573", "1A8370", "17806C", "157E69", "127C66", "0F7A63", "0D7860"],
+      colours = ["90E5FD", "8DE2F9", "8AE0F6", "87DEF3", "85DCF0", "82D9EC", "7FD7E9", "7DD5E6", "7AD3E3", "77D0E0", "75CEDC", "72CCD9", "6FCAD6", "6DC8D3", "6AC5D0", "67C3CC", "65C1C9", "62BFC6", "5FBCC3", "5DBAC0", "5AB8BC", "57B6B9", "55B4B6", "52B1B3", "4FAFB0", "4DADAC", "4AABA9", "47A8A6", "45A6A3", "42A4A0", "3FA29C", "3DA099", "3A9D96", "379B93", "359990", "32978C", "2F9489", "2D9286", "2A9083", "278E80", "258C7C", "228979", "1F8776", "1D8573", "1A8370", "17806C", "157E69", "127C66", "0F7A63", "0D7860"], // TODO: Generate this array from start_colour and end_colour. See Asciano.
       colour = 0,
       cw,
       ch;
 
   window.addEventListener("resize", resize_canvas, false);
   resize_canvas();
-  setInterval(update_colour, 40);
+  inc_colour();
 
   document.getElementById("content").addEventListener("click", function(e) {
     var cell = cell_for(e.x, e.y);
@@ -38,18 +38,24 @@
     ch = ((window.innerHeight - 10) / h).toFixed(1);
   }
 
-  function update_colour() {
-    colour += 1
+  function inc_colour() {
+    colour += 1;
 
     if (colour >= colours.length) {
-      colour = 0;
+      setTimeout(dec_colour, 100);
+    } else {
+      setTimeout(inc_colour, 100);
     }
   }
 
-  function colour_for_population(alive) {
-    var normalised = Math.round(((alive - 1) / (((w*h) / 10) - 1)) * 50);
+  function dec_colour() {
+    colour -= 1;
 
-    return colours[normalised];
+    if (colour <= 0) {
+      setTimeout(inc_colour, 100);
+    } else {
+      setTimeout(dec_colour, 100);
+    }
   }
 
   function build_world() {
@@ -111,8 +117,6 @@
       fjs.each(function(c){ new_state[c] = 1; }, mixin);
       mixin = [];
     }
-
-    //colour = colour_for_population(alive);
 
     setTimeout(function(){step(new_state);}, r);
   })(build_world());
