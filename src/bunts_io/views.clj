f9d56e7ea8fe361065604c4bcf5858c281409713
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-04-26 14:58:09 +1000

    Some swanky ass css shit

--- src/bunts_io/views.clj
+++ src/bunts_io/views.clj
@@ -12,9 +12,9 @@
         [:div {:id "nav"}
           [:h1 "Bunts.io"]
           [:ul {:id "menu"}
-            [:li [:a {:href "http://codrspace.com/buntine/"} "Blog"]]
-            [:li [:a {:href "http://github.com/buntine/"} "Projects"]]
-            [:li [:a {:href "#" :id "contact"} "Contact"]
+            [:li [:a {:href "http://codrspace.com/buntine/"} "BLOG"]]
+            [:li [:a {:href "http://github.com/buntine/"} "PROJECTS"]]
+            [:li [:a {:href "#" :id "contact"} "CONTACT"]
                  [:span {:id "swagmail"} "&#105;&#110;&#102;&#111;&#064;&#098;&#117;&#110;&#116;&#115;&#046;&#105;&#111;"]]]]]]
      (include-css "/css/gol.css")
      (include-css "/css/main.css")
