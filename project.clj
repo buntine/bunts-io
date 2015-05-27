510cc6887f5f5e4bdb2f64eaa67f634725c4ae54
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-27 23:10:57 +1000

    Adding in experiments

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]
+                 [selmer "0.8.2"]
                  [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]
             [lein-beanstalk "0.2.7"]]
--- src/bunts_io/handler.clj
+++ src/bunts_io/handler.clj
@@ -2,10 +2,14 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [bunts-io.views :as views]
+            [selmer.parser :as sp]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
   (GET "/" [] (views/home-page))
+  (GET "/experiments/:exp_name" [exp_name]
+    (sp/render-file (str "public/raw/" exp_name) {}))
+
   (route/not-found "Not Found"))
 
 (def app
