e094382927b5c4d6ef21bef057a2ad5aeccbde81
Andrew 'Von Doom' Buntine <info@bunts.io>
2017-01-08 13:15:39 +1100

    Fixed resume

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bunts-io "0.1.3-SNAPSHOT"
+(defproject bunts-io "0.1.4-SNAPSHOT"
   :description "Personal website of Andrew Buntine"
   :url "http://bunts.io/"
   :min-lein-version "2.0.0"
